9ee95fac2822198880f2fb5a9ac2dcfe92365a99
Alejo Lopez <alejolopezdemichelis@gmail.com>
2022-09-02 11:24:25 -0300

    optimized flyback code
    Para optimizar el uso de recursos se cambio el protocolo del GPS de NMEA a UBX y se usa una librería mas ligera.

--- src/mCALCAN_FlyBack/mCALCAN_FlyBack.ino
+++ src/mCALCAN_FlyBack/mCALCAN_FlyBack.ino
@@ -1,23 +1,43 @@
-#include "CustomQMC5883L.h"
+#include <CustomQMC5883L.h>
 #include <Wire.h>
-#include <TinyGPS++.h>
-#include <TinyGPSPlus.h>
 #include <Servo.h>
+#include <UbxGpsNavPosllh.h>
 
+#define GPS_BAUDRATE 115200
 #define DRDYPIN 3
 #define servoDerPin 10
 #define servoIzqPin 9
+#define servoDerMaxAngle 0
+#define servoDerMinAngle 180
+#define servoIzqMaxAngle 180
+#define servoIzqMinAngle 0
+#define compassYawCorrection 0
 
+
+/*
+El modulo gps se debe configurar desde U-center, cambiando:
+* el baudrate de 9600 a 115200
+* el protocolo de salida de NMEA+UBX a UBX
+* se debe activar en cfg-msg el mensaje nav-posllh en uart1
+* cambiar el rate a 100ms o 200ms
+
+IMPORTANTE: guardar la configuracion del gps en el icono que se un engranaje con el simbolo de guardado
+
+  Usar el codigo en extras->serial-bridge-gps.ino para conectar el modulo a la pc
+*/
+
+/*
+IMPORTANTE: para que el compas funcione correctamente hay que calibrarlo ver: extras->Calibracion compas. Luego cargar los datos de calibracion en CustomQMC583L.h, en las variables calibration matrix y bias
+*/
 CustomQMC5883L compass;
-TinyGPSPlus TINY_GPS_OBJECT;
+UbxGpsNavPosllh<HardwareSerial> gps(Serial); //cambiar a serial2 para usar con el bluepill
 Servo servoIzq;
 Servo servoDer;
 
-const int servoDerMaxAngle = 0, servoDerMinAngle = 180;
-const int servoIzqMaxAngle = 180, servoIzqMinAngle = 0;
-float magX, magY, magZ;
-double miLatitud, miLongitud, destinoLatitud = -31.380368, destinoLongitud = -64.243142, miVelocidad, miOrientacion, orientacionADestino;
-float deltaOrientacion, distanciaADestino;
+
+
+double miLatitud, miLongitud, destinoLatitud = -31.380368, destinoLongitud = -64.243142;
+int8_t deltaOrientacion, distanciaADestino, miOrientacion, orientacionADestino;
 
 
 
@@ -33,8 +53,9 @@ void setup() {
 }
 
 void loop() {
-  leerGPS();
-  leerCompass();
+    leerCompass();
+    leerGPS();
+
 }
 
 void compassSetup() {
@@ -42,7 +63,7 @@ void compassSetup() {
     Serial.println("Fallo al conectarse al modulo compass");
     delay(250);
   }
-  compass.useCalibration(true);
+  //compass.useCalibration(true); 
   compass.setSmoothingSteps(5);
   pinMode(DRDYPIN, INPUT_PULLUP);
   // attachInterrupt(digitalPinToInterrupt(DRDYPIN), leerCompass, CHANGE);
@@ -51,58 +72,43 @@ void compassSetup() {
 
 void leerCompass() {
   compass.read();
-  magX = compass.getX();
-  magY = compass.getY();
-  magZ = compass.getZ();
-  miOrientacion = compass.getAzimuth();
+  miOrientacion = compass.getAzimuth() - compassYawCorrection;
   deltaOrientacion = miOrientacion - orientacionADestino;
 }
 
 
 
 void GPSSetup() {
-  /* Serial.begin(9600);
-  while (!Serial)
-    ;
-  Serial.println("CFG-UART1-BAUDRATE 0x1C200");
-  Serial.end();
-*/
-
-  Serial.begin(115200);
-  digitalWrite(4, HIGH);
-  Serial.println("CFG-RATE-MEAS 0xc8");
-  delay(1000);
-  digitalWrite(4, LOW);
-
+  gps.begin(GPS_BAUDRATE);
+  
   Serial.println("GPS ready");
 }
 
 void leerGPS() {
 
-  while (Serial.available()) {
-    if (TINY_GPS_OBJECT.encode(Serial.read())) {
-      miLatitud = TINY_GPS_OBJECT.location.lat();
-      miLongitud = TINY_GPS_OBJECT.location.lng();
-      miVelocidad = TINY_GPS_OBJECT.speed.mps();
-      distanciaADestino = (unsigned long)TinyGPSPlus::distanceBetween(miLatitud, miLongitud, destinoLatitud, destinoLongitud);
-      orientacionADestino = (unsigned long)TinyGPSPlus::courseTo(miLatitud, miLongitud, destinoLatitud, destinoLongitud);
+
+  if (gps.ready()) {
+
+    miLongitud = (gps.lon / 10000000.0);
+
+    miLatitud = (gps.lat / 10000000.0);
+
+    distanciaADestino = distanceBetween(miLatitud, miLongitud, destinoLatitud, destinoLongitud);
+    orientacionADestino = courseTo(miLatitud, miLongitud, destinoLatitud, destinoLongitud);
 
 
-      Serial.print("LAT = ");
-      Serial.println(miLatitud, 6);
-      Serial.print("LONG = ");
-      Serial.println(miLongitud, 6);
-      Serial.print("SPEED = ");
-      Serial.println(miVelocidad);
-      Serial.print("COURSE TO DESTINATION = ");
-      Serial.println(orientacionADestino, 2);
-      Serial.print("DISTANCE TO DESTINATION = ");
-      Serial.println(distanciaADestino, 2);
-      Serial.print("Orientacion: ");
-      Serial.println(miOrientacion);
-      Serial.print("Delta orientacion: ");
-      Serial.println(deltaOrientacion);
-    }
+    Serial.print("LAT = ");
+    Serial.println(miLatitud, 6);
+    Serial.print("LONG = ");
+    Serial.println(miLongitud, 6);
+    Serial.print("COURSE TO DESTINATION = ");
+    Serial.println(orientacionADestino, 2);
+    Serial.print("DISTANCE TO DESTINATION = ");
+    Serial.println(distanciaADestino, 2);
+    Serial.print("Orientacion: ");
+    Serial.println(miOrientacion);
+    Serial.print("Delta orientacion: ");
+    Serial.println(deltaOrientacion);
   }
 }
 
@@ -116,4 +122,65 @@ void servoSetup() {
   Serial.println("Servo ready");
 }
 void servoControl() {
+
+
+
 }
+
+int distanceBetween(double lat1, double long1, double lat2, double long2) {
+  // returns distance in meters between two positions, both specified
+  double delta = radians(long1 - long2);
+  double sdlong = sin(delta);
+  double cdlong = cos(delta);
+  lat1 = radians(lat1);
+  lat2 = radians(lat2);
+  double slat1 = sin(lat1);
+  double clat1 = cos(lat1);
+  double slat2 = sin(lat2);
+  double clat2 = cos(lat2);
+  delta = (clat1 * slat2) - (slat1 * clat2 * cdlong);
+  delta = sq(delta);
+  delta += sq(clat2 * sdlong);
+  delta = sqrt(delta);
+  double denom = (slat1 * slat2) + (clat1 * clat2 * cdlong);
+  delta = atan2(delta, denom);
+  return delta * 6372795;
+}
+
+int courseTo(double lat1, double long1, double lat2, double long2) {
+  // returns course in degrees (North=0, West=270) from position 1 to position 2,
+
+  double dlon = radians(long2 - long1);
+  lat1 = radians(lat1);
+  lat2 = radians(lat2);
+  double a1 = sin(dlon) * cos(lat2);
+  double a2 = sin(lat1) * cos(lat2) * cos(dlon);
+  a2 = cos(lat1) * sin(lat2) - a2;
+  a2 = atan2(a1, a2);
+  if (a2 < 0.0) {
+    a2 += TWO_PI;
+  }
+  return degrees(a2);
+}
+
+
+void stopGPS()
+{
+  byte packet[] = {0xB5,0x62,0x06,0x04,0x04,0x00,0x00,0x00,0x08,0x00,0x16,0x74};
+    for (byte i = 0; i < sizeof(packet); i++)
+    {
+        Serial.write(packet[i]);
+    }
+
+ }
+
+void startGPS()
+{
+    byte packet[] = {0xB5,0x62,0x06,0x04,0x04,0x00,0x00,0x00,0x09,0x00,0x17,0x76};
+    for (byte i = 0; i < sizeof(packet); i++)
+    {
+        Serial.write(packet[i]);
+    }
+
+}
+
